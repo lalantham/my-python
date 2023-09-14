c284032a473f3a39abd1b00d8da05e5ceab9ec34
lalantham <lalantham82@gmail.com>
2023-09-14 11:13:47 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, World!"
+    return '<h1>Hello from Azure App Service</h1>'
+
+if __name__ == '__main__':
+    app.run()
